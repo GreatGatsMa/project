cfe0debec59cd9685c3f2fb605e89a50700e95d6
GreatGatsMa <GreatGatsMa@gmail.com>
2019-09-02 19:42:16 +0800

    3

---
+++ src/components/tabbar/my.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        4
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+div{
+    width: 100%;
+    height: 100px;
+    background-color: red;
+}
+</style>
